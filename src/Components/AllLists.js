9afc42a186bdf04f84a20044df77fbe4f2be1b35
Tobias Norton <tnorton20@yahoo.com>
2019-04-02 16:15:17 -0400

    Cleans up JSON object

--- src/Components/AllLists.js
+++ src/Components/AllLists.js
@@ -6,15 +6,65 @@ class AllLists extends Component {
     super(props)
 
     this.state = {
-      lists: []
+      lists: [
+        {
+          listName: 'Job Search',
+          listItems: [
+            {
+              task: "I'm going to do something.",
+              details: ["Yea that's cool", "Okay man let's have fun", 'I love to code']
+            },
+            {
+              task: 'Need to do another thing.',
+              details: ['Hi, Mom!', 'Detail about this thing', 'I love to code']
+            },
+            {
+              task: 'Third thing I have to do.',
+              details: ["Yea that's cool", "Okay man let's have fun", 'I love to code']
+            }
+          ]
+        },
+        {
+          listName: 'House Stuff',
+          listItems: [
+            {
+              task: 'Clean up the yard',
+              details: ['Put yard tools away', 'Mow the lawn', 'Weed the garden']
+            },
+            {
+              task: 'Clean up inside',
+              details: ['Throw away old food items', 'Wipe out fridge', 'Dishes']
+            }
+          ]
+        },
+        {
+          listName: 'Car Stuff',
+          listItems: [
+            {
+              task: 'Clean car',
+              details: ['Car wash', 'Vacuum interior', 'Wipe down vinyl', 'Wax']
+            },
+            {
+              task: 'Maintenance',
+              details: ['New power steering pump', 'Tires']
+            }
+          ]
+        }
+      ]
     }
   }
+
+  // getLists = () => {
+  //   return this.state.lists.map((list, index) => {
+  //     return (
+  //       <List key={index} listName={list.listName} list
+  //     )
+  //   })
+  // }
 
   render() {
     return (
       <div className="all-lists">
-        <List />
-        <List />
         <List />
         <List />
         <List />
